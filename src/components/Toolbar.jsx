d8f6795f0cb14f0025245725d5b19fc94ece6991
serviktor050 <mr.s-gagulin@yandex.ru>
2021-02-17 09:43:21 +0300

    Внесены корректировки в решение

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -3,16 +3,12 @@ import React from "react";
 export default function Toolbar(props) {
   const { filters, selected } = props;
 
-  const onSelectFilter = (e) => {
-    props.onSelectFilter(e.target.textContent);
-  };
-
   return (
     <div className="filters">
       {filters.map((filter) => {
         return (
           <div
-            onClick={onSelectFilter}
+            onClick={() => props.onSelectFilter(filter)}
             className={filter === selected && "active"}
           >
             {filter}
